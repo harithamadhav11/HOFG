3228805b02c4e1296339472d9cac185bd46c496b
Haritha Madhav C <111914002@smail.iitpkd.ac.in>
2021-12-31 12:01:53 +0530

    added switch block

--- HOFG.cpp
+++ HOFG.cpp
@@ -1,4 +1,4 @@
-//===- Hofg.cpp - Code for generating HOFG ---------------===//
+//===- HOFG.cpp - Code for generating HOFG ---------------===//
 //
 //
 
@@ -15,6 +15,7 @@
 #include <llvm/ADT/BreadthFirstIterator.h>
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
+#include "HOFG.def"
 #include <set>
 #include <cstdlib>
 using namespace llvm;
@@ -27,9 +28,38 @@ namespace {
 	struct HOFG : public ModulePass {
         static char ID;
 	    HOFG() : ModulePass(ID) {}
+        enum vertexType {obj,ptr,snk,oos};
+        struct V{
+            Value *name;
+            vertexType *type;
+        };
+        struct F {
+            V *head;
+            V *tail;
+        };
+        struct R {
+            V *head;
+            V *tail;
+        };
+        struct D {
+            V *head;
+            V *tail;
+        };
+        struct HOFGraph {
+            std::set<V> vertices;
+            std::set<F> flows;
+            std::set<R> derefs;
+            std::set<D> derived;
+        };
+        struct E {
+            std::set<F> flows;
+            std::set<R> derefs;
+            std::set<D> derived;
+        };
 	    bool runOnModule(Module &M) override {
             errs()<<"Entered module pass";
             constructHOFG(M);
+            traverseCallGraph(M);
             return true;
 	    };
         void constructHOFG(Module &M) {
@@ -48,16 +78,115 @@ namespace {
         void idRelevantCodeSegment(BasicBlock &B) {
             for(BasicBlock::iterator BI=B.begin(); BI!=B.end(); BI++) {
                 Instruction &I(*BI);
+                if(isMallocFunction(I)) {
+                    handleRelevantCodeSegment(MEM_ALLOC, B);                    
+                }
+                if(identifyCopyInstruction(I)) {
+                    handleRelevantCodeSegment(K_COPY, B);
+                }
+                if(identifyLoadInstruction(I)) {
+                    handleRelevantCodeSegment(LK_DEREF,B);
+                }
+                if(identifyStoreInstruction(I)) {
+                    handleRelevantCodeSegment(STORE,B);
+                }
                 handleRelevantCodeSegment(I.getOpcode(), B);
             }
+        }
+        bool isMallocFunction(Instruction &I) {
+            if(isa<BitCastInst>(I)) {
+                if(isa<CallInst>(I.getOperand(0))) {
+                    CallInst *call=dyn_cast<CallInst>(I.getOperand(0));
+                    Function *F = call->getCalledFunction();
+                    if(F->getName() == MALLOC) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        void identifyCopyInstruction(Instruction &I) {
+            //store preceeded by load, for pointer types
+            //p=q
+            if(StoreInst *storIns = dyn_cast<StoreInst>(&I)){
+                if(isa<LoadInst>(storIns->getOperand(0)) && isa<PointerType>(storIns->getOperand(0)->getType())) {
+                    LoadInst *old = dyn_cast<LoadInst>(storIns->getOperand(0));
+                    if(isa<LoadInst>(old->getOperand(0)) || isa<LoadInst>(storIns->getOperand(1))) {
+                        //Not a copy it is either a load or store constraint
+                    } else {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        void identifyLoadInstruction(Instruction &I) {
+            if(StoreInst *storIns = dyn_cast<StoreInst>(&I)){
+                if(isa<LoadInst>(storIns->getOperand(0)) && isa<PointerType>(storIns->getOperand(0)->getType())) {
+                    if(isa<LoadInst>(storIns->getOperand(1)) && isa<PointerType>(storIns->getOperand(1)->getType())) {
+                        errs()<<"Load of two pointer types\n";
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        void identifyStoreInstruction(Instruction &I) {
+            if(StoreInst *storIns = dyn_cast<StoreInst>(&I)){
+                if(isa<LoadInst>(storIns->getOperand(0)) && isa<PointerType>(storIns->getOperand(0)->getType())) {
+                    Instruction* load1=dyn_cast<Instruction>(storIns->getOperand(0));
+                    if(isa<LoadInst>(load1->getOperand(0)) && isa<PointerType>(load1->getOperand(0)->getType())) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
 
         void handleRelevantCodeSegment(int option, BasicBlock &B) {
             errs()<<"\n Reached in handler : "<< B.getName();
-            errs()<<"\n function name: "<< B.getParent()->getName();
-
+///            errs()<<"\n function name: "<< B.getParent()->getName();
+            switch (option) {
+                case MEM_ALLOC: errs()<<"\nfound malloc";
+                                break;
+                case MEM_DEALLOC : errs()<<"\nfound dealloc";
+                                break;
+                case K_COPY : errs()<<"copy into a known pointer";
+                                break;
+                case U_COPY : errs()<<"copy into an unknown pointer";
+                                break;
+                case DEREF : errs()<<"\n found a dereference";
+                                break;
+                case LK_DEREF : errs()<<"\nload with known dereference";
+                                break;
+                case LU_DEREF : errs()<<"\nload with unknown dereference";
+                                break;
+                case STORE : errs()<<"\nstore instruction";
+                                break;
+                default : errs()<<"\ninvalid instruction";
+            }
         }
-        
+        void traverseCallGraph(Module &M) {
+            //for (CallGraph::iterator CGI=CallGraph(M).begin(); CGI!=CallGraph(M).end(); CGI++) {
+            for (auto &CGI : CallGraph(M)) {
+                errs()<<"\nlooped once";
+                if (const Function *F = CGI.first) {
+                    errs()<<"\n Function name : "<<F->getName();
+                    errs()<<"\n number of references : "<< CGI.second->getNumReferences();
+                    for(auto &Vec : CGI.second->CalledFunctionsVector) {
+                        errs()<<"\n are for Function " <<Vec.second->getFunction()->getName();
+                    }
+                } else {
+
+                }
+                errs()<<"\n.........................................\n";
+            }
+            errs()<<"\nDumping call graph\n";
+            CallGraph(M).dump();
+        }
         void getAnalysisUsage(AnalysisUsage &AU) const override {
           AU.setPreservesAll();
         }
